afba3f3c09c9b513bda16a1f71c333e75accecaa
katrinajelinek <kmrjelinek@gmail.com>
2020-10-27 11:10:08 -0600

    adds user signup

---
+++ src/views/Signup.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="signup">
+    <form v-on:submit.prevent="submit()">
+      <h1>Signup</h1>
+      <ul>
+        <li class="text-danger" v-for="error in errors">{{ error }}</li>
+      </ul>
+      <div class="form-group">
+        <label>First name:</label> 
+        <input type="text" class="form-control" v-model="firstName">
+      </div>
+      <div class="form-group">
+        <label>Last name:</label> 
+        <input type="text" class="form-control" v-model="lastName">
+      </div>
+      <div class="form-group">
+        <label>Email:</label>
+        <input type="email" class="form-control" v-model="email">
+      </div>
+      <div class="form-group">
+        <label>Password:</label>
+        <input type="password" class="form-control" v-model="password">
+      </div>
+      <div class="form-group">
+        <label>Password confirmation:</label>
+        <input type="password" class="form-control" v-model="passwordConfirmation">
+      </div>
+      <div class="form-group">
+        <label>Image:</label> 
+        <input type="text" class="form-control" v-model="imageUrl">
+      </div>
+      <input type="submit" class="btn btn-primary" value="Submit">
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      imageUrl: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        first_name: this.firstName,
+        last_name: this.lastName,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation,
+        image_url: this.imageUrl
+      };
+      axios
+        .post("/api/users", params)
+        .then(response => {
+          this.$router.push("/login");
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors;
+        });
+    }
+  }
+};
+</script>
