9c321444084b09dd805a627225e9c29208248a65
katrinajelinek <kmrjelinek@gmail.com>
2020-11-14 17:41:42 -0700

    adds html form and button

--- src/views/PostsNew.vue
+++ src/views/PostsNew.vue
@@ -1,41 +1,179 @@
 <template>
   <div class="posts-new">
-    <form v-on:submit.prevent="createPost()">
+    <div class="users-edit">
+      <div class="replay-box">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="container">
+              <h3>Post a clipping</h3>
+              <ul>
+                <li class="text-danger" v-for="error in errors">{{ error }}</li>
+              </ul>
+              <form
+                id="comment-form"
+                class="row"
+                name="comment-form"
+                method="post"
+                v-on:submit.prevent="createPost()"
+              >
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      name="name"
+                      class="form-control"
+                      required="required"
+                      placeholder="Plant Type"
+                      v-model="plantType"
+                    />
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      name="name"
+                      class="form-control"
+                      required="required"
+                      placeholder="Trade for"
+                      v-model="tradeFor"
+                    />
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      name="name"
+                      class="form-control"
+                      required="required"
+                      placeholder="Description"
+                      v-model="description"
+                    />
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      name="name"
+                      class="form-control"
+                      required="required"
+                      placeholder="Your location"
+                      v-model="location"
+                    />
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <div>
+                      <multiselect
+                        v-model="values"
+                        :options="tags"
+                        :multiple="true"
+                        :close-on-select="false"
+                        :clear-on-select="false"
+                        :preserve-search="true"
+                        placeholder="Tags"
+                        label="name"
+                        track-by="name"
+                        :preselect-first="true"
+                      >
+                        <template
+                          slot="selection"
+                          slot-scope="{ values, isOpen }"
+                          ><span
+                            class="multiselect__single"
+                            v-if="values.length &amp;&amp; !isOpen"
+                            >{{ values.length }} tags selected</span
+                          ></template
+                        >
+                      </multiselect>
+                      <!-- <pre class="language-json"><code>{{ value  }}</code></pre> -->
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="form-group">
+                    <input
+                      type="file"
+                      name="name"
+                      class="form-control"
+                      required="required"
+                      v-on:change="setFile($event)"
+                      ref="fileInput"
+                    />
+                  </div>
+                </div>
+                <div class="col-sm-12 form-group">
+                  <button type="submit" class="btn btn-primary pull-right">
+                    Post your clipping
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- <form v-on:submit.prevent="createPost()">
       <h1>New Clipping</h1>
       <ul>
         <li class="text-danger" v-for="error in errors">{{ error }}</li>
       </ul>
       <div class="form-group">
-        <label>Plant type:</label> 
-        <input type="text" class="form-control" v-model="plantType">
+        <label>Plant type:</label>
+        <input type="text" class="form-control" v-model="plantType" />
       </div>
       <div class="form-group">
-        <label>Trade for:</label> 
-        <input type="text" class="form-control" v-model="tradeFor">
+        <label>Trade for:</label>
+        <input type="text" class="form-control" v-model="tradeFor" />
       </div>
       <div class="form-group">
-        <label>Description</label> 
-        <input type="text" class="form-control" v-model="description">
+        <label>Description</label>
+        <input type="text" class="form-control" v-model="description" />
       </div>
       <div class="form-group">
-        <label>Location:</label> 
-        <input type="text" class="form-control" v-model="location">
+        <label>Location:</label>
+        <input type="text" class="form-control" v-model="location" />
       </div>
       <div class="form-group">
-        <label>Image:</label> 
-        <input type="file" class="form-control" v-on:change="setFile($event)" ref="fileInput">
+        <label>Image:</label>
+        <input
+          type="file"
+          class="form-control"
+          v-on:change="setFile($event)"
+          ref="fileInput"
+        />
       </div>
-      <input type="submit" class="btn btn-primary" value="Submit">
-    </form>
+      <input type="submit" class="btn btn-primary" value="Submit" />
+    </form> -->
 
-<!-- tag multiselect -->
-    <div>
+    <!-- tag multiselect -->
+    <!-- <div>
       <label class="typo__label">Select Tags</label>
-      <multiselect v-model="values" :options="tags" :multiple="true" :close-on-select="false" :clear-on-select="false" :preserve-search="true" placeholder="Pick some" label="name" track-by="name" :preselect-first="true">
-        <template slot="selection" slot-scope="{ values, isOpen }"><span class="multiselect__single" v-if="values.length &amp;&amp; !isOpen">{{ values.length }} tags selected</span></template>
+      <multiselect
+        v-model="values"
+        :options="tags"
+        :multiple="true"
+        :close-on-select="false"
+        :clear-on-select="false"
+        :preserve-search="true"
+        placeholder="Pick some"
+        label="name"
+        track-by="name"
+        :preselect-first="true"
+      >
+        <template slot="selection" slot-scope="{ values, isOpen }"
+          ><span
+            class="multiselect__single"
+            v-if="values.length &amp;&amp; !isOpen"
+            >{{ values.length }} tags selected</span
+          ></template
+        >
       </multiselect>
-      <!-- <pre class="language-json"><code>{{ value  }}</code></pre> -->
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -45,7 +183,7 @@ import Multiselect from "vue-multiselect";
 
 export default {
   components: {
-    Multiselect
+    Multiselect,
   },
   data: function() {
     return {
@@ -84,17 +222,17 @@ export default {
         .then((response) => {
           this.$router.push("/posts");
         })
-        .catch(error => {
+        .catch((error) => {
           this.errors = error.response.data.errors;
         });
     },
-    indexTags: function () {
-      axios.get("/api/tags").then(response => {
+    indexTags: function() {
+      axios.get("/api/tags").then((response) => {
         console.log(response.data);
         this.tags = response.data;
       });
     },
-  }
+  },
 };
 </script>
 
