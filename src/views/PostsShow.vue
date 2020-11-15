3368dce8c7e08bdfa8c4ddc768b534b41df9bbc7
katrinajelinek <kmrjelinek@gmail.com>
2020-11-14 19:11:06 -0700

    adds html to show post and display offers and create offer

--- src/views/PostsShow.vue
+++ src/views/PostsShow.vue
@@ -1,100 +1,278 @@
 <template>
-  <div class="posts-show">
-<!-- post info -->
-    <img :src="post.image_url" alt="" class="image-fit">
-    <h1>{{post.plant_type}}</h1>
+  <div id="sub-page" class="posts-show">
+    <div id="main-blog">
+      <div class="container padding-top">
+        <div class="row section-title text-center">
+          <div class="col-sm-8 col-sm-offset-2">
+            <h1>Our Blog</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              ac augue at erat hendrerit dictum. Praesent porta, purus eget
+              sagittis imperdiet.
+            </p>
+          </div>
+        </div>
+        <div class="row">
+          <div id="content" class="full-width site-content col-md-12">
+            <div class="post">
+              <div class="col-sm-12">
+                <div class="entry-thumbnail">
+                  <img class="img-responsive" :src="post.image_url" alt="" />
+                </div>
+              </div>
+              <div class="post-content">
+                <h2 class="entry-title">
+                  <h1>Clipped by:</h1>
+                  <router-link :to="`/users/${post.user_id}`">
+                    <h3>
+                      {{ post.user.first_name }} {{ post.user.last_name }}
+                    </h3>
+                  </router-link>
+                </h2>
+                <div class="entry-summary">
+                  <h3>Trade for: {{ post.trade_for }}</h3>
+                  <p>Description: {{ post.description }}</p>
+                  <p>Location: {{ post.location }}</p>
+                  <h3>Tags:</h3>
+                  <div v-for="tag in post.tags">
+                    <p>{{ tag.name }}</p>
+                  </div>
+                  <p>Created {{ relativeDate(post.created_at) }}</p>
+                  <div v-if="$parent.getUserId() == post.user_id">
+                    <router-link :to="`/posts/${post.id}/edit`"
+                      >Edit</router-link
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+            <!--/post-->
+            <div class="media-wrapper">
+              <div class="comments-area">
+                <h2 class="comments-heading">Offers</h2>
+                <ul class="media-list">
+                  <li class="media" v-for="offer in post.offers">
+                    <div class="post-comment">
+                      <router-link
+                        class="pull-left"
+                        :to="`/users/${offer.user_id}`"
+                      >
+                        <img
+                          class="media-object"
+                          :src="offer.user.image_url"
+                          alt=""
+                        />
+                      </router-link>
+                      <div class="media-body">
+                        <h3>
+                          <router-link :to="`/users/${offer.user_id}`">
+                            {{ offer.user.first_name }}
+                            {{ offer.user.last_name }}
+                          </router-link>
+                          said {{ relativeDate(offer.created_at) }}
+                        </h3>
+                        <h5>
+                          {{ offer.message }}
+                        </h5>
+                        <img :src="offer.image_url" alt="" />
+                      </div>
+                    </div>
+                    <div v-if="$parent.getUserId() == post.user_id">
+                      <div v-if="!post.offer_accepted">
+                        <button
+                          v-on:click="updateOfferAccept(offer)"
+                          class="btn btn-primary pull-right"
+                        >
+                          Accept Offer
+                        </button>
+                      </div>
+                      <div v-if="offer.accepted === true">
+                        <p>
+                          {{ offer.user.first_name }}
+                          {{ offer.user.last_name }}'s email:
+                          {{ offer.user.email }}
+                        </p>
+                      </div>
+                    </div>
+                    <div v-if="$parent.getUserId() == offer.user_id">
+                      <button
+                        v-on:click="offerEditAuthentication = offer.id"
+                        class="btn btn-primary pull-left"
+                      >
+                        Edit Offer
+                      </button>
+                    </div>
+                  </li>
+                  <br />
+                  <br />
+                </ul>
+              </div>
+              <!--/comments-area-->
+              <div class="replay-box">
+                <div class="row">
+                  <div class="col-sm-12">
+                    <h3>Leave an Offer</h3>
+                    <ul>
+                      <li class="text-danger" v-for="error in errors">
+                        {{ error }}
+                      </li>
+                    </ul>
+                    <form
+                      v-on:submit.prevent="createOffer()"
+                      id="comment-form"
+                      class="row"
+                      name="comment-form"
+                      method="post"
+                    >
+                      <div class="col-sm-6">
+                        <div class="form-group">
+                          <textarea
+                            name="message"
+                            id="message"
+                            required="required"
+                            class="form-control"
+                            rows="7"
+                            placeholder="Your Message"
+                            v-model="message"
+                          ></textarea>
+                        </div>
+                      </div>
+                      <div class="col-sm-6">
+                        <div class="form-group">
+                          <input
+                            type="file"
+                            name="name"
+                            class="form-control"
+                            v-on:change="setFile($event)"
+                            ref="fileInput"
+                          />
+                        </div>
+                      </div>
+                      <div class="col-sm-12 form-group">
+                        <button
+                          type="submit"
+                          class="btn btn-primary pull-right"
+                        >
+                          Place Offer
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+              <!--/Repaly Box-->
+            </div>
+            <!--/media-wrapper-->
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--/#main-blog-->
+
+    <!-- post info -->
+    <!-- <img :src="post.image_url" alt="" class="image-fit" /> -->
+    <h1>{{ post.plant_type }}</h1>
     <div v-if="post.offer_accepted">
       <h3>An offer has been accepted</h3>
     </div>
-    <h3>Clipped by:</h3>
+    <!-- <h3>Clipped by:</h3>
     <router-link :to="`/users/${post.user_id}`">
-      <h3>{{post.user.first_name}} {{post.user.last_name}}</h3>
-    </router-link>
-    <h2>Trade for: {{post.trade_for}}</h2>
-    <p>Description: {{post.description}}</p>
-    <p>Location: {{post.location}}</p>
+      <h3>{{ post.user.first_name }} {{ post.user.last_name }}</h3>
+    </router-link> -->
+    <!-- <h2>Trade for: {{ post.trade_for }}</h2>
+    <p>Description: {{ post.description }}</p>
+    <p>Location: {{ post.location }}</p>
     <h3>Tags:</h3>
     <div v-for="tag in post.tags">
-      <p>{{tag.name}}</p>
+      <p>{{ tag.name }}</p>
     </div>
-    <p>Created {{relativeDate(post.created_at)}}</p>
+    <p>Created {{ relativeDate(post.created_at) }}</p> -->
     <div v-if="$parent.getUserId() == post.user_id">
       <router-link :to="`/posts/${post.id}/edit`">Edit</router-link>
-    </div> 
-      
+    </div>
 
-<!-- offers index -->
+    <!-- offers index -->
     <h2>Offers</h2>
     <div v-for="offer in post.offers">
-      <router-link :to="`/users/${offer.user_id}`">
-        <h3>{{offer.user.first_name}} {{offer.user.last_name}}</h3>
-      </router-link>
+      <!-- <router-link :to="`/users/${offer.user_id}`">
+        <h3>{{ offer.user.first_name }} {{ offer.user.last_name }}</h3>
+      </router-link> -->
       <div v-if="offer.accepted === true">
-        <h3>This offer has been accepted</h3> 
+        <h3>This offer has been accepted</h3>
       </div>
-      <img :src="offer.image_url" alt="" class="image-fit">
-      <p>Message: {{offer.message}}</p>
-      <p>Created {{relativeDate(offer.created_at)}}</p>
+      <!-- <img :src="offer.image_url" alt="" class="image-fit" /> -->
+      <!-- <p>Message: {{ offer.message }}</p>
+      <p>Created {{ relativeDate(offer.created_at) }}</p> -->
 
-
-<!-- accept and edit offer buttons-->
-      <div v-if="$parent.getUserId() == post.user_id">
+      <!-- accept and edit offer buttons-->
+      <!-- <div v-if="$parent.getUserId() == post.user_id">
         <div v-if="!post.offer_accepted">
           <button v-on:click="updateOfferAccept(offer)">
             Accept Offer
           </button>
         </div>
         <div v-if="offer.accepted === true">
-          <p>{{offer.user.first_name}} {{offer.user.last_name}}'s email: {{offer.user.email}}</p>
+          <p>
+            {{ offer.user.first_name }} {{ offer.user.last_name }}'s email:
+            {{ offer.user.email }}
+          </p>
         </div>
       </div>
       <div v-if="$parent.getUserId() == offer.user_id">
         <button v-on:click="offerEditAuthentication = offer.id">
           Edit
-        </button>
+        </button> -->
 
-
-<!-- update offer -->
-        <div v-if="offerEditAuthentication === offer.id">
-          <form v-on:submit.prevent="updateOffer(offer)">
-            <h2>Update Offer:</h2>
-            <ul>
-              <li class="text-danger" v-for="error in errors">{{ error }}</li>
-            </ul>
-            <div class="form-group">
-              <label>Message:</label> 
-              <input type="text" class="form-control" v-model="offer.message">
-            </div>
-            <div>
-              <label>Image:</label> 
-              <input type="file" class="form-control" v-on:change="setFile($event)" ref="fileInput">
-            </div>
-            <input type="submit" class="btn btn-primary" value="Update">
-          </form>
-        </div>
-        <button v-on:click="destroyOffer(offer)">
-          Delete
-        </button>
+      <!-- update offer -->
+      <div v-if="offerEditAuthentication === offer.id">
+        <form v-on:submit.prevent="updateOffer(offer)">
+          <h2>Update Offer:</h2>
+          <ul>
+            <li class="text-danger" v-for="error in errors">{{ error }}</li>
+          </ul>
+          <div class="form-group">
+            <label>Message:</label>
+            <input type="text" class="form-control" v-model="offer.message" />
+          </div>
+          <div>
+            <label>Image:</label>
+            <input
+              type="file"
+              class="form-control"
+              v-on:change="setFile($event)"
+              ref="fileInput"
+            />
+          </div>
+          <input type="submit" class="btn btn-primary" value="Update" />
+        </form>
       </div>
+      <button v-on:click="destroyOffer(offer)">
+        Delete
+      </button>
     </div>
 
-<!-- new offer -->
-    <form v-on:submit.prevent="createOffer()">
+    <!-- new offer -->
+    <!-- <form v-on:submit.prevent="createOffer()">
       <h2>Place an Offer:</h2>
       <ul>
         <li class="text-danger" v-for="error in errors">{{ error }}</li>
       </ul>
       <div class="form-group">
-        <label>Message:</label> 
-        <input type="text" class="form-control" v-model="message">
+        <label>Message:</label>
+        <input type="text" class="form-control" v-model="message" />
       </div>
       <div>
-        <label>Image:</label> 
-        <input id="imageInput" type="file" class="form-control" v-on:change="setFile($event)" ref="fileInput">
+        <label>Image:</label>
+        <input
+          id="imageInput"
+          type="file"
+          class="form-control"
+          v-on:change="setFile($event)"
+          ref="fileInput"
+        />
       </div>
-      <input type="submit" class="btn btn-primary" value="Submit">
-    </form>
-
+      <input type="submit" class="btn btn-primary" value="Submit" />
+    </form> -->
   </div>
 </template>
 
@@ -108,12 +286,12 @@ export default {
   data: function() {
     return {
       post: {
-        user: {}
+        user: {},
       },
       errors: [],
       offerEditAuthentication: null,
       message: "",
-      image: ""
+      image: "",
     };
   },
   created: function() {
@@ -123,13 +301,13 @@ export default {
     updateOfferAccept: function(offer) {
       var formData = new FormData();
       formData.append("accepted", true);
-      axios.patch(`/api/offers/${offer.id}`, formData).then(response => {
+      axios.patch(`/api/offers/${offer.id}`, formData).then((response) => {
         console.log(response.data);
         offer.accepted = true;
         this.post.offer_accepted = true;
       });
     },
-    relativeDate: function (date) {
+    relativeDate: function(date) {
       return moment(date).fromNow();
     },
     setFile: function(event) {
@@ -137,14 +315,14 @@ export default {
         this.image = event.target.files[0];
       }
     },
-    showPost: function () {
-      axios.get(`/api/posts/${this.$route.params.id}`).then(response => {
+    showPost: function() {
+      axios.get(`/api/posts/${this.$route.params.id}`).then((response) => {
         console.log(response.data);
         this.post = response.data;
       });
     },
-    destroyPost: function () {
-      axios.delete(`/api/posts/${this.post.id}`).then(response => {
+    destroyPost: function() {
+      axios.delete(`/api/posts/${this.post.id}`).then((response) => {
         console.log("Success", response.data);
         this.$router.push("/posts");
       });
@@ -163,11 +341,11 @@ export default {
           this.message = "";
           document.getElementById("imageInput").value = "";
         })
-        .catch(error => {
+        .catch((error) => {
           this.errors = error.response.data.errors;
         });
     },
-    updateOffer: function (offer) {
+    updateOffer: function(offer) {
       var formData = new FormData();
       var index = this.post.offers.indexOf(offer);
       formData.append("message", offer.message);
@@ -178,16 +356,16 @@ export default {
       axios
         .patch(`/api/offers/${offer.id}`, formData)
         .then((response) => {
-          this.post.offers[index] = (response.data);
+          this.post.offers[index] = response.data;
           this.offerEditAuthentication = null;
         })
-        .catch(error => {
+        .catch((error) => {
           this.errors = error.response.data.errors;
         });
     },
-    destroyOffer: function (offer) {
+    destroyOffer: function(offer) {
       var index = this.post.offers.indexOf(offer);
-      axios.delete(`/api/offers/${offer.id}`).then(response => {
+      axios.delete(`/api/offers/${offer.id}`).then((response) => {
         console.log("Success", response.data);
         this.post.offers.splice(index, 1);
       });
