edbd258ee2d94be2c9622d4f976854ccc381b032
katrinajelinek <kmrjelinek@gmail.com>
2020-11-16 11:00:48 -0700

    changes home route to be posts index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+// import Home from '../views/Home.vue';
 import Signup from '../views/Signup.vue';
 import Login from '../views/Login.vue';
 import Logout from '../views/Logout.vue';
@@ -14,19 +14,11 @@ import PostsEdit from '../views/PostsEdit.vue';
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
   {
     path: '/signup',
     name: 'signup',
@@ -53,7 +45,7 @@ const routes = [
     component: UsersEdit
   },
   {
-    path: '/posts',
+    path: '/',
     name: 'posts-index',
     component: PostsIndex
   },
