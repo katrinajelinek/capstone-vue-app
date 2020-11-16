849a75c40df20358d906e3ff6b871407e9240f88
katrinajelinek <kmrjelinek@gmail.com>
2020-11-16 11:01:19 -0700

    changes home route to be posts index

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-// import Home from '../views/Home.vue';
 import Signup from '../views/Signup.vue';
 import Login from '../views/Login.vue';
 import Logout from '../views/Logout.vue';
@@ -14,11 +13,6 @@ import PostsEdit from '../views/PostsEdit.vue';
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
   {
     path: '/signup',
     name: 'signup',
