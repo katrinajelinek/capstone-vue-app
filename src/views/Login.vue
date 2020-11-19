cc214863902ca852b1d268485812b369bce862f6
katrinajelinek <kmrjelinek@gmail.com>
2020-11-19 08:01:10 -0700

    adds padding to bottom of form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,51 +3,53 @@
     <div id="our-team">
       <!-- our-team -->
       <div class="container text-center our-team padding-top padding-bottom">
-        <div class="row section-title text-center">
-          <div class="col-sm-8 col-sm-offset-2">
-            <h1>Login</h1>
+        <div class="container text-center our-team padding-bottom">
+          <div class="row section-title text-center">
+            <div class="col-sm-8 col-sm-offset-2">
+              <h1>Login</h1>
+            </div>
           </div>
+          <ul>
+            <li class="text-danger" v-for="error in errors">{{ error }}</li>
+          </ul>
+          <form
+            id="comment-form"
+            class="row"
+            name="comment-form"
+            method="post"
+            v-on:submit.prevent="submit()"
+          >
+            <div class="col-sm-12">
+              <div class="form-group">
+                <input
+                  type="email"
+                  name="name"
+                  class="form-control"
+                  required="required"
+                  placeholder="Email"
+                  v-model="email"
+                />
+              </div>
+            </div>
+            <div class="col-sm-12">
+              <div class="form-group">
+                <input
+                  type="password"
+                  name="name"
+                  class="form-control"
+                  required="required"
+                  placeholder="Password"
+                  v-model="password"
+                />
+              </div>
+            </div>
+            <div class="col-sm-12 form-group">
+              <button type="submit" class="btn btn-primary pull-right">
+                Login
+              </button>
+            </div>
+          </form>
         </div>
-        <ul>
-          <li class="text-danger" v-for="error in errors">{{ error }}</li>
-        </ul>
-        <form
-          id="comment-form"
-          class="row"
-          name="comment-form"
-          method="post"
-          v-on:submit.prevent="submit()"
-        >
-          <div class="col-sm-12">
-            <div class="form-group">
-              <input
-                type="email"
-                name="name"
-                class="form-control"
-                required="required"
-                placeholder="Email"
-                v-model="email"
-              />
-            </div>
-          </div>
-          <div class="col-sm-12">
-            <div class="form-group">
-              <input
-                type="password"
-                name="name"
-                class="form-control"
-                required="required"
-                placeholder="Password"
-                v-model="password"
-              />
-            </div>
-          </div>
-          <div class="col-sm-12 form-group">
-            <button type="submit" class="btn btn-primary pull-right">
-              Login
-            </button>
-          </div>
-        </form>
       </div>
       <!-- #/ our-team -->
     </div>
